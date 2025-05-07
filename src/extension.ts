2028c38aa25ceecef9af717240f2c797117ceaf1
Sebastian Provenzano <seprov@gmail.com>
2025-05-07 14:23:10 -0500

    remove unused imports

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,6 @@ import {
 	DecorationRenderOptions,
 	DiagnosticChangeEvent,
 	DiagnosticSeverity,
-	DocumentColorProvider,
-	DocumentFormattingEditProvider,
 	env,
 	EventEmitter,
 	ExtensionContext,
