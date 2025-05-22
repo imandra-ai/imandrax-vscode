d8d586097a559794d17548c0841a74eb775318dd
seprov <95832216+seprov@users.noreply.github.com>
2025-05-22 15:53:34 -0500

    S/use new install script (#36)

---
+++ src/apiKey.ts
@@ -0,0 +1,34 @@
+import { Uri, workspace } from 'vscode';
+import { homedir } from 'os';
+
+const CONFIG_DIR = ['.config', 'imandrax'] as const;
+const API_KEY_FILE = 'api_key';
+
+const readOptionalFile = async (file: Uri) => {
+  try {
+    const res = await workspace.fs.readFile(file);
+    return Buffer.from(res.buffer).toString();
+  } catch (e) {
+    return null;
+  }
+};
+
+function getApiKeyFileUri(): Uri {
+  const home = Uri.file(homedir());
+  const configDir = Uri.joinPath(home, ...CONFIG_DIR);
+  return Uri.joinPath(configDir, API_KEY_FILE);
+}
+
+export async function get(): Promise<string | null> {
+  const userHome = Uri.file(homedir());
+  return (process.env.IMANDRA_API_KEY ||
+    (await readOptionalFile(
+      getApiKeyFileUri()
+    )))?.trim();
+}
+
+export async function put(key: string): Promise<void> {
+  const file = getApiKeyFileUri();
+  await workspace.fs.createDirectory(file.with({ path: file.path.replace(/\/[^/]+$/, '') })); // parent dir
+  await workspace.fs.writeFile(file, Buffer.from(key, 'utf8')); // overwrite OK
+}
---
+++ src/environment.ts
@@ -0,0 +1,67 @@
+import * as Which from "which";
+import {
+  env,
+  workspace,
+  WorkspaceConfiguration
+} from "vscode";
+
+interface PlatformConfiguration {
+  onWindows: boolean;
+  inRemoteWsl: boolean;
+}
+
+interface ImandraXLspConfiguration {
+  serverArgs,
+  mergedEnv,
+  binAbsPath: BinAbsPath,
+}
+
+type BinAbsPath =
+  | {
+    status: "foundPath"
+    path: string
+  }
+  | {
+    status: "onWindows"
+  }
+  | {
+    status: "missingPath"
+  }
+
+function getBinAbsPath(platform_configuration: PlatformConfiguration, binary: string): BinAbsPath {
+  if ((!platform_configuration.onWindows)
+    || (platform_configuration.onWindows && platform_configuration.inRemoteWsl)) {
+    const path = Which.sync(binary, { nothrow: true });
+    if (path != "" && path != null) {
+      return { status: "foundPath", path };
+    }
+    else {
+      return { status: "missingPath" };
+    }
+  } else {
+    return { status: "onWindows" };
+  }
+}
+
+function getPlatformConfiguration(): PlatformConfiguration {
+  const onWindows = process.platform === "win32";
+  const inRemoteWsl = env.remoteName === "wsl";
+
+  return { onWindows, inRemoteWsl };
+}
+
+export function getEnv(): ImandraXLspConfiguration {
+  const config = workspace.getConfiguration("imandrax");
+  const binary = config.lsp.binary;
+  const serverArgs = config.lsp.arguments;
+  const serverEnv = config.lsp.environment;
+
+  const systemEnv = process.env;
+  const mergedEnv = Object.assign(systemEnv, serverEnv);
+
+  const platformConfiguration = getPlatformConfiguration();
+
+  const binAbsPath = getBinAbsPath(platformConfiguration, binary);
+
+  return { serverArgs, mergedEnv, binAbsPath };
+}
