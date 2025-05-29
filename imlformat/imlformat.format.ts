3c3de8dcf29af1ff89a6320f1ef29a0637a284a2
Christoph M. Wintersteiger <christoph@imandra.ai>
2025-05-29 19:18:34 +0100

    More

--- imlformat/imlformat.format.ts
+++ imlformat/imlformat.format.ts
@@ -4,16 +4,16 @@ import { Buffer } from 'node:buffer';
 import * as prettier from 'prettier';
 import * as iml_prettier from './iml-prettier';
 
-export async function format(text: string): Promise<string> {
+export async function format(text: string, config_root: string = null): Promise<string> {
   try {
-    const config_file = await prettier.resolveConfigFile();
+    const config_file = await prettier.resolveConfigFile(config_root);
     let options: prettier.Options | null = null;
     if (config_file)
       options = await prettier.resolveConfig(config_file);
     else {
       options = {
         semi: false
-      }
+      };
     }
     options.parser = "iml-parse";
     options.plugins = [iml_prettier];
@@ -29,5 +29,5 @@ export async function format_file(filename: string): Promise<string> {
   const buffer = Buffer.allocUnsafe(stats.size);
   const fd = fs.openSync(filename, "r");
   fs.readSync(fd, buffer, 0, buffer.length, 0);
-  return await format(buffer.toString());
+  return await format(buffer.toString(), filename);
 }
--- imlformat/test/comments.test.ts
+++ imlformat/test/comments.test.ts
@@ -12,6 +12,16 @@ let f = 1
 let f = 1`))
 })
 
+test("Comment 2", () => {
+  format(`
+let f = 1
+(* This is a comment *)
+`).then(x =>
+    expect(x).toEqual(`\
+(* This is a comment *)
+let f = 1`))
+})
+
 test("Docstring 1", () => {
   format(`
 (** This is a docstring *)
@@ -20,4 +30,23 @@ let f = 1
     expect(x).toEqual(`\
 let f = 1
 (** This is a docstring *)`))
+})
+
+test("Floating docstring", () => {
+  format(`
+  let f
+  =
+  1
+
+(** This is a docstring *)
+
+    let
+g = 1
+`).then(x =>
+    expect(x).toEqual(`\
+let f = 1
+
+(** This is a docstring *)
+
+let g = 1`))
 })
