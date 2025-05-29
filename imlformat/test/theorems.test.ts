234ffbab5660d698c07e163898043901e5ed0c2d
Christoph M. Wintersteiger <christoph@imandra.ai>
2025-05-29 18:47:07 +0100

    More

---
+++ imlformat/test/theorems.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@jest/globals';
+
+import { format } from "../imlformat.format";
+
+test("theorem 1", () => {
+  format(`
+    let f x = (x) + (1)
+
+theorem
+   thm1     x y z
+   = ((f  x   ) >
+    x)
+    && ((f  y   ) > y)
+    && ((f  z   ) > z)
+    [@@timeout 3600]
+  [@@disable f] [@@by [%expand "f"] @> auto]
+  [@@by
+    some
+      other
+        tactic]
+
+`).then(x =>
+    expect(x).toEqual(`\
+let f x = x + 1
+
+theorem thm1 x y z =
+  ((f x) > x) && (((f y) > y) && ((f z) > z))
+[@@timeout 3600]
+[@@disable f]
+[@@by [%expand "f"] @> auto]
+[@@by some other tactic]`))
+})
