2ffd16cdccc988c399c1fcf709b3fc9c83697f06
Christoph M. Wintersteiger <christoph@imandra.ai>
2025-06-12 22:22:01 +0100

    More tweaks and fixes

---
+++ imlformat/test/verify.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@jest/globals';
+
+import { format } from "../imlformat.format";
+
+test("verify 1", () => {
+  format(`\
+verify (fun x -> 1 <> 2) [@@by ground_eval] [@@timeout 2]`
+  ).then(x => expect(x).toEqual(`\
+verify (fun x -> 1 <> 2)
+[@@by ground_eval]
+[@@timeout 2]`
+  ))
+});
