f4410fa8f06570b38e238edfd3baab13c27ad945
Sebastian Provenzano <seprov@gmail.com>
2025-06-16 15:23:04 -0500

    replacte eslintrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 
 export default [{
   files: ["**/*.ts"],
@@ -19,7 +19,12 @@ export default [{
       selector: "import",
       format: ["camelCase", "PascalCase"],
     }],
-
+    "sort-imports": ["error", { "ignoreCase": true }],
+    'semi': [2, "always"],
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
     curly: "warn",
     eqeqeq: "warn",
     "no-throw-literal": "warn",
