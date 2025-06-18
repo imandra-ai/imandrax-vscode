dc1ed47d636638e279f0d1b66fa86b9cf590ca31
Sebastian Provenzano <seprov@gmail.com>
2025-06-18 14:10:50 -0500

    fix path 2

--- src/test/commands.lsp.test.ts
+++ src/test/commands.lsp.test.ts
@@ -8,11 +8,14 @@ import * as sqlite3 from 'sqlite3';
 import * as util from '../util';
 import * as vscode from 'vscode';
 
-const root = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-export const dbPath = path.join(root!, '..', '..', '.vscode-test', 'db', 'imandrax.sqlite');
 
 suite('Commands, LSP Test Suite', () => {
+  let dbPath: string;
   suiteTeardown(async () => {
+    const root = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+    dbPath = root
+      ? path.join(root!, '..', '..', '.vscode-test', 'db', 'imandrax.sqlite')
+      : await fs.mkdtemp(path.join(os.tmpdir(), 'imandrax-'));;
     await fs.rm(dbPath, { force: true });
     vscode.window.showInformationMessage('All tests done!');
   });
