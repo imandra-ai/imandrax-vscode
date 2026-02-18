9ba560f35e819ce6e99c5048969a5ec5dffe86c0
Christoph M. Wintersteiger <christoph@imandra.ai>
2026-02-18 16:29:15 +0000

    Adjust comment

--- src/test/commands.test.ts
+++ src/test/commands.test.ts
@@ -16,7 +16,8 @@ import { ImandraXLanguageClient } from '../imandrax_language_client/imandrax_lan
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function set_workspace_config(workspaceDir: string) {
-  // For manual testing purposes we may want to change the LSP configuration.
+  // For manual testing purposes we may want to change the LSP configuration
+  // along these lines:
 
   console.log("Opening workspace");
 
