b9c9347b488bc52efbcff99eeaefd6cbb130563a
Christoph M. Wintersteiger <christoph@imandra.ai>
2026-04-10 14:57:09 +0100

    Fix one-concl turnstiles

--- src/goal-state/term-formatter.ts
+++ src/goal-state/term-formatter.ts
@@ -573,7 +573,7 @@ class TermFormatter {
 
     let doc: Doc = this.term2doc(t);
     if (this._with_turnstile)
-      doc = fill([kw("&#x22A2;"), text(" "), doc]);
+      doc = g([kw("&#x22A2;"), indent([line, doc])]);
     const r = pretty(this._width, doc);
     // const end = performance.now();
     // console.log(`Prettifier time: ${(end - start) / 1000.0} sec, ${r.length} characters, width ${this._width}`);
